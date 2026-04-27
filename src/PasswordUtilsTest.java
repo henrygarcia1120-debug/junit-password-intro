c1544c5aa8a5249fd327a50c66cfe36aa3c3ae91
Henry Garcia <garcia.henry@student.greenriver.edu>
2026-04-27 11:25:01 -0700

    code from livecode in class

--- src/PasswordUtilsTest.java
+++ src/PasswordUtilsTest.java
@@ -1,8 +1,37 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordUtilsTest {
     @Test
-    void testDescribePasswordLengthShortPassword() {
-
+    void testDescribePasswordLengthShortPasswordEmptyString() {
+        // Arrange
+        String password = "";
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        // Assert
+        assertEquals("short", actual);
     }
+
+    @Test
+    void testDescribePassWordLengthLongPassword() {
+        // Arrange
+        String password = "eeniemeeniemienemoe";
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        // Assert 
+        assertEquals("long", actual);
+    }
+
+    @Test
+    void testPasswordLongerThan40charsIsVeryLong() {
+        // Arrange
+        String password = "qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm";
+        // Act
+        String actual = PasswordUtils.describePasswordLength(password);
+        // Assert
+        assertEquals("very long", actual);
+    }
+    @Test 
+    void testIs
 }
